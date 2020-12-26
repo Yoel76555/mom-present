69649d7900c5c21d95c42b5938e8fc78b0ba8411
Yoel76555 <71801083+Yoel76555@users.noreply.github.com>
2020-12-25 20:02:43 -0700

    Add files via upload

---
+++ mom/sketch.js
@@ -0,0 +1,33 @@
+ var mom;
+
+
+function preload(){
+ backgroundImg=loadImage("background.jpg");
+}
+
+
+function setup() {
+  createCanvas(400,400);
+
+   
+  
+    
+}
+
+function draw() {
+ background(backgroundImg);
+  
+  textSize(14);
+  text("DEAR MOM", 160,200);
+  text("Mom my Queen You have been there for me since I was born. ", 10, 220);
+  text("I can't thank you enough for raising me.", 5, 240);
+  text("You deserve the world and so much more.", 5, 260);
+  text("I don't know what I would do with out you.", 5, 280);
+  text("All I can do is make you proud.",5, 300);
+  text(" From Yoel Your King.", 5, 320);
+   
+  
+  
+  drawSprites();
+  
+}
